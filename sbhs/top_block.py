4932a2a0cbc14cc89dd876fce5a648ff4d5d7a64
Mushirahmed <imushir@gmail.com>
2015-01-09 11:28:24 +0530

    grc files

--- sbhs/top_block.py
+++ sbhs/top_block.py
@@ -2,7 +2,7 @@
 ##################################################
 # Gnuradio Python Flow Graph
 # Title: Top Block
-# Generated: Fri Jan  9 10:57:15 2015
+# Generated: Fri Jan  2 10:24:11 2015
 ##################################################
 
 from gnuradio import eng_notation
@@ -10,10 +10,10 @@ from gnuradio import gr
 from gnuradio.eng_option import eng_option
 from gnuradio.gr import firdes
 from gnuradio.wxgui import forms
-from gnuradio.wxgui import numbersink2
 from grc_gnuradio import wxgui as grc_wxgui
 from optparse import OptionParser
-import gnuradio.power
+from sbhs import plot_sink
+import gnuradio.sbhs.sbfan as sbfan
 import wx
 
 class top_block(grc_wxgui.top_block_gui):
@@ -24,36 +24,12 @@ class top_block(grc_wxgui.top_block_gui):
 		##################################################
 		# Variables
 		##################################################
-		self.variable_slider_1 = variable_slider_1 = 2
-		self.variable_slider_0 = variable_slider_0 = 2
+		self.variable_slider_0 = variable_slider_0 = 30
 		self.samp_rate = samp_rate = 32000
 
 		##################################################
 		# Blocks
 		##################################################
-		_variable_slider_1_sizer = wx.BoxSizer(wx.VERTICAL)
-		self._variable_slider_1_text_box = forms.text_box(
-			parent=self.GetWin(),
-			sizer=_variable_slider_1_sizer,
-			value=self.variable_slider_1,
-			callback=self.set_variable_slider_1,
-			label='variable_slider_1',
-			converter=forms.float_converter(),
-			proportion=0,
-		)
-		self._variable_slider_1_slider = forms.slider(
-			parent=self.GetWin(),
-			sizer=_variable_slider_1_sizer,
-			value=self.variable_slider_1,
-			callback=self.set_variable_slider_1,
-			minimum=0,
-			maximum=100,
-			num_steps=100,
-			style=wx.SL_HORIZONTAL,
-			cast=float,
-			proportion=1,
-		)
-		self.Add(_variable_slider_1_sizer)
 		_variable_slider_0_sizer = wx.BoxSizer(wx.VERTICAL)
 		self._variable_slider_0_text_box = forms.text_box(
 			parent=self.GetWin(),
@@ -77,43 +53,30 @@ class top_block(grc_wxgui.top_block_gui):
 			proportion=1,
 		)
 		self.Add(_variable_slider_0_sizer)
-		self.wxgui_numbersink2_0 = numbersink2.number_sink_f(
+		self.sbfan_0 = sbfan.sbfan(1,0,0)
+		self.plot_sink_0 = plot_sink.plot_sink_f(
 			self.GetWin(),
-			unit="Units",
-			minval=-100,
-			maxval=100,
-			factor=1.0,
-			decimal_places=10,
-			ref_level=0,
-			sample_rate=samp_rate,
-			number_rate=15,
-			average=False,
-			avg_alpha=None,
-			label="Number Plot",
-			peak_hold=False,
-			show_gauge=True,
+			title="Scope Plot",
+			vlen=1,
+			decim=1,
+			gsz=1000,
+			zoom=0,
 		)
-		self.Add(self.wxgui_numbersink2_0.win)
-		self.power_xy_power_0 = gnuradio.power.power()
-		self.const_source_x_1 = gr.sig_source_f(0, gr.GR_CONST_WAVE, 0, 0, variable_slider_1)
-		self.const_source_x_0 = gr.sig_source_f(0, gr.GR_CONST_WAVE, 0, 0, variable_slider_0)
+		self.Add(self.plot_sink_0.win)
+		self.gr_sig_source_x_0 = gr.sig_source_f(samp_rate, gr.GR_SIN_WAVE, 1000, 30, 0)
+		self.gr_delay_0 = gr.delay(gr.sizeof_float*1, 5)
+		self.gr_add_const_vxx_0 = gr.add_const_vff((30, ))
+		self.const_source_x_0 = gr.sig_source_f(0, gr.GR_CONST_WAVE, 0, 0, 40)
 
 		##################################################
 		# Connections
 		##################################################
-		self.connect((self.const_source_x_0, 0), (self.power_xy_power_0, 0))
-		self.connect((self.const_source_x_1, 0), (self.power_xy_power_0, 1))
-		self.connect((self.power_xy_power_0, 0), (self.wxgui_numbersink2_0, 0))
+		self.connect((self.const_source_x_0, 0), (self.sbfan_0, 0))
+		self.connect((self.gr_sig_source_x_0, 0), (self.gr_add_const_vxx_0, 0))
+		self.connect((self.sbfan_0, 0), (self.plot_sink_0, 0))
+		self.connect((self.gr_add_const_vxx_0, 0), (self.gr_delay_0, 0))
+		self.connect((self.gr_delay_0, 0), (self.sbfan_0, 1))
 
-
-	def get_variable_slider_1(self):
-		return self.variable_slider_1
-
-	def set_variable_slider_1(self, variable_slider_1):
-		self.variable_slider_1 = variable_slider_1
-		self.const_source_x_1.set_offset(self.variable_slider_1)
-		self._variable_slider_1_slider.set_value(self.variable_slider_1)
-		self._variable_slider_1_text_box.set_value(self.variable_slider_1)
 
 	def get_variable_slider_0(self):
 		return self.variable_slider_0
@@ -122,13 +85,13 @@ class top_block(grc_wxgui.top_block_gui):
 		self.variable_slider_0 = variable_slider_0
 		self._variable_slider_0_slider.set_value(self.variable_slider_0)
 		self._variable_slider_0_text_box.set_value(self.variable_slider_0)
-		self.const_source_x_0.set_offset(self.variable_slider_0)
 
 	def get_samp_rate(self):
 		return self.samp_rate
 
 	def set_samp_rate(self, samp_rate):
 		self.samp_rate = samp_rate
+		self.gr_sig_source_x_0.set_sampling_freq(self.samp_rate)
 
 if __name__ == '__main__':
 	parser = OptionParser(option_class=eng_option, usage="%prog: [options]")
--- sbhs/top_block_rises.py
+++ sbhs/top_block_rises.py
@@ -2,7 +2,7 @@
 ##################################################
 # Gnuradio Python Flow Graph
 # Title: Top Block
-# Generated: Fri Jan  9 10:57:15 2015
+# Generated: Fri Dec 26 17:12:03 2014
 ##################################################
 
 from gnuradio import eng_notation
@@ -10,10 +10,10 @@ from gnuradio import gr
 from gnuradio.eng_option import eng_option
 from gnuradio.gr import firdes
 from gnuradio.wxgui import forms
-from gnuradio.wxgui import numbersink2
 from grc_gnuradio import wxgui as grc_wxgui
 from optparse import OptionParser
-import gnuradio.power
+from sbhs import plot_sink
+import gnuradio.sbhs.sbfan as sbfan
 import wx
 
 class top_block(grc_wxgui.top_block_gui):
@@ -24,28 +24,28 @@ class top_block(grc_wxgui.top_block_gui):
 		##################################################
 		# Variables
 		##################################################
-		self.variable_slider_1 = variable_slider_1 = 2
-		self.variable_slider_0 = variable_slider_0 = 2
+		self.variable_slider_0_0 = variable_slider_0_0 = 25
+		self.variable_slider_0 = variable_slider_0 = 15
 		self.samp_rate = samp_rate = 32000
 
 		##################################################
 		# Blocks
 		##################################################
-		_variable_slider_1_sizer = wx.BoxSizer(wx.VERTICAL)
-		self._variable_slider_1_text_box = forms.text_box(
+		_variable_slider_0_0_sizer = wx.BoxSizer(wx.VERTICAL)
+		self._variable_slider_0_0_text_box = forms.text_box(
 			parent=self.GetWin(),
-			sizer=_variable_slider_1_sizer,
-			value=self.variable_slider_1,
-			callback=self.set_variable_slider_1,
-			label='variable_slider_1',
+			sizer=_variable_slider_0_0_sizer,
+			value=self.variable_slider_0_0,
+			callback=self.set_variable_slider_0_0,
+			label='variable_slider_0_0',
 			converter=forms.float_converter(),
 			proportion=0,
 		)
-		self._variable_slider_1_slider = forms.slider(
+		self._variable_slider_0_0_slider = forms.slider(
 			parent=self.GetWin(),
-			sizer=_variable_slider_1_sizer,
-			value=self.variable_slider_1,
-			callback=self.set_variable_slider_1,
+			sizer=_variable_slider_0_0_sizer,
+			value=self.variable_slider_0_0,
+			callback=self.set_variable_slider_0_0,
 			minimum=0,
 			maximum=100,
 			num_steps=100,
@@ -53,7 +53,7 @@ class top_block(grc_wxgui.top_block_gui):
 			cast=float,
 			proportion=1,
 		)
-		self.Add(_variable_slider_1_sizer)
+		self.Add(_variable_slider_0_0_sizer)
 		_variable_slider_0_sizer = wx.BoxSizer(wx.VERTICAL)
 		self._variable_slider_0_text_box = forms.text_box(
 			parent=self.GetWin(),
@@ -77,43 +77,36 @@ class top_block(grc_wxgui.top_block_gui):
 			proportion=1,
 		)
 		self.Add(_variable_slider_0_sizer)
-		self.wxgui_numbersink2_0 = numbersink2.number_sink_f(
+		self.sbfan_0 = sbfan.sbfan(1,variable_slider_0_0,variable_slider_0)
+		self.plot_sink_0 = plot_sink.plot_sink_f(
 			self.GetWin(),
-			unit="Units",
-			minval=-100,
-			maxval=100,
-			factor=1.0,
-			decimal_places=10,
-			ref_level=0,
-			sample_rate=samp_rate,
-			number_rate=15,
-			average=False,
-			avg_alpha=None,
-			label="Number Plot",
-			peak_hold=False,
-			show_gauge=True,
+			title="Scope Plot",
+			vlen=1,
+			decim=1,
+			gsz=50,
+			zoom=0,
 		)
-		self.Add(self.wxgui_numbersink2_0.win)
-		self.power_xy_power_0 = gnuradio.power.power()
-		self.const_source_x_1 = gr.sig_source_f(0, gr.GR_CONST_WAVE, 0, 0, variable_slider_1)
+		self.Add(self.plot_sink_0.win)
+		self.const_source_x_0_0 = gr.sig_source_f(0, gr.GR_CONST_WAVE, 0, 0, variable_slider_0_0)
 		self.const_source_x_0 = gr.sig_source_f(0, gr.GR_CONST_WAVE, 0, 0, variable_slider_0)
 
 		##################################################
 		# Connections
 		##################################################
-		self.connect((self.const_source_x_0, 0), (self.power_xy_power_0, 0))
-		self.connect((self.const_source_x_1, 0), (self.power_xy_power_0, 1))
-		self.connect((self.power_xy_power_0, 0), (self.wxgui_numbersink2_0, 0))
+		self.connect((self.const_source_x_0, 0), (self.sbfan_0, 0))
+		self.connect((self.const_source_x_0_0, 0), (self.sbfan_0, 1))
+		self.connect((self.sbfan_0, 0), (self.plot_sink_0, 0))
 
 
-	def get_variable_slider_1(self):
-		return self.variable_slider_1
+	def get_variable_slider_0_0(self):
+		return self.variable_slider_0_0
 
-	def set_variable_slider_1(self, variable_slider_1):
-		self.variable_slider_1 = variable_slider_1
-		self.const_source_x_1.set_offset(self.variable_slider_1)
-		self._variable_slider_1_slider.set_value(self.variable_slider_1)
-		self._variable_slider_1_text_box.set_value(self.variable_slider_1)
+	def set_variable_slider_0_0(self, variable_slider_0_0):
+		self.variable_slider_0_0 = variable_slider_0_0
+		self._variable_slider_0_0_slider.set_value(self.variable_slider_0_0)
+		self._variable_slider_0_0_text_box.set_value(self.variable_slider_0_0)
+		self.const_source_x_0_0.set_offset(self.variable_slider_0_0)
+		self.sbfan_0.set_fan_heat(self.variable_slider_0_0,self.variable_slider_0)
 
 	def get_variable_slider_0(self):
 		return self.variable_slider_0
@@ -123,6 +116,7 @@ class top_block(grc_wxgui.top_block_gui):
 		self._variable_slider_0_slider.set_value(self.variable_slider_0)
 		self._variable_slider_0_text_box.set_value(self.variable_slider_0)
 		self.const_source_x_0.set_offset(self.variable_slider_0)
+		self.sbfan_0.set_fan_heat(self.variable_slider_0_0,self.variable_slider_0)
 
 	def get_samp_rate(self):
 		return self.samp_rate
